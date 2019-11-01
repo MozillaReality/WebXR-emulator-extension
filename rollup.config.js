19e22993b820403aac87dfabd3fdc46d2eaaa850
Takahiro <hogehoge@gachapin.jp>
2019-11-01 16:57:58 -0700

    Import the official WebXR polyfill

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+import commonjs from 'rollup-plugin-commonjs';
+import replace from 'rollup-plugin-replace';
+import resolve from 'rollup-plugin-node-resolve';
+import cleanup from 'rollup-plugin-cleanup';
+
+export default {
+  input: 'src/CustomWebXRPolyfill.js',
+  output: {
+    file: './polyfill/webxr-polyfill.js',
+    format: 'umd',
+    name: 'CustomWebXRPolyfill',
+    // Note: These banner and footer are the trick to inject polyfill in content-script
+    banner: 'function WebXRPolyfillInjection() {',
+    footer: '}'
+  },
+  plugins: [
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
+    resolve(),
+    commonjs(),
+    cleanup({
+      comments: 'none',
+    }),
+  ],
+};
---
+++ src/CustomWebXRPolyfill.js
@@ -0,0 +1,64 @@
+import WebXRPolyfill from 'webxr-polyfill/src/WebXRPolyfill';
+import API from 'webxr-polyfill/src/api/index';
+import EmulatedXRDevice from './EmulatedXRDevice';
+
+export default class CustomWebXRPolyfill extends WebXRPolyfill {
+  constructor() {
+    super();
+
+    // Note: Even if native WebXR API is available the extension overrides
+    //       it with WebXR polyfill because the extension doesn't work with
+    //       the native one (yet).
+    if (this.nativeWebXR) {
+      console.log('WebXR emulator extension overrides native WebXR API with polyfill.');
+    }
+
+    // Note: Regardless whether native WebXR API is available we set up WebXR API in global scope
+    //       by using Object.defineProperties() with writable:false because
+    //       1. In case native WebXR API is available.
+    //          We override them with WebXR polyfill as commented above
+    //       2. In case native WebXR API isn't available, meaning WebXR API polyfill is injected in
+    //          parent constructor.
+    //          The WebXR API polyfill can be overriden by native WebXR API on Chrome
+    //          after CustomWebXRPolyfill is instanciated if WebXR origin-trial is set in a web page.
+    //          To prevent the override we define WebXR API again with writable:false in glocal scope.
+    const defines = {};
+    for (const className of Object.keys(API)) {
+      defines[className] = {
+        value: API[className],
+        configurable: false,
+        writable: false
+      };
+    }
+    Object.defineProperties(this.global, defines);
+
+    if (this.nativeWebXR) {
+      this.injected = true;
+      this._patchNavigatorXR();
+    }
+  }
+
+  _patchNavigatorXR() {
+    const devicePromise = requestXRDevice(this.global);
+    this.xr = new XR(devicePromise);
+    Object.defineProperty(this.global.navigator, 'xr', {
+      value: this.xr,
+      configurable: true,
+    });
+  }
+}
+
+const requestXRDevice = async function (global, config) {
+  // resolve when receiving configuration parameters from content-script as an event
+  return new Promise((resolve, reject) => {
+    const callback = (event) => {
+      window.removeEventListener('webxr-device-init', callback);
+      resolve(new EmulatedXRDevice(global,
+        Object.assign({},
+          event.detail.deviceDefinition,
+          {stereoEffect: event.detail.stereoEffect}
+        )));
+    };
+    window.addEventListener('webxr-device-init', callback, false);
+  });
+};
