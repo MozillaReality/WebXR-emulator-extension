9a350edac3349e7d2de0ebcdd642c113bbaa0998
Takahiro <hogehoge@gachapin.jp>
2019-11-11 16:52:22 -0800

    Update workaround for Chrome and WebXR origin trial

--- src/CustomWebXRPolyfill.js
+++ src/CustomWebXRPolyfill.js
@@ -42,35 +42,50 @@ export default class CustomWebXRPolyfill extends WebXRPolyfill {
       }
     });
 
-    // Note: Even if native WebXR API is available the extension overrides
-    //       it with WebXR polyfill because the extension doesn't work with
-    //       the native one (yet).
     if (this.nativeWebXR) {
-      console.log('WebXR emulator extension overrides native WebXR API with polyfill.');
-    }
-
-    // Note: Regardless whether native WebXR API is available we set up read-only WebXR API
-    //       in global scope by using Object.defineProperties() because
-    //       1. In case native WebXR API is available.
-    //          We override them with WebXR polyfill as commented above
-    //       2. In case native WebXR API isn't available, meaning WebXR API polyfill is injected in
-    //          parent constructor.
-    //          The WebXR API polyfill can be overriden by native WebXR API on Chrome
-    //          after CustomWebXRPolyfill is instanciated if WebXR origin-trial is set in a web page.
-    //          To prevent the override we define WebXR API again as read-only in global scope.
-    const defines = {};
-    for (const className of Object.keys(API)) {
-      defines[className] = {
-        // We don't use value because window.foo = bar will throw an error in strict mode
-        get: () => { return API[className]; },
-        set: (value) => {}
-      };
-    }
-    Object.defineProperties(this.global, defines);
-
-    if (this.nativeWebXR) {
+      // Note: Even if native WebXR API is available the extension overrides
+      //       it with WebXR polyfill because the extension doesn't work with
+      //       the native one (yet).  　　　　overrideAPI(this.global);
       this.injected = true;
       this._patchNavigatorXR();
+    } else {
+      // Note: WebXR API polyfill can be overridden by native WebXR API on the latest Chrome 78
+      //       after the extension is loaded but before loading page is completed
+      //       if the native WebXR API is disabled via chrome://flags and the page includes
+      //       WebXR origin trial.
+      //       Here is a workaround. Check if XR class is native code when node is appended or
+      //       the page is loaded. If it detects, override WebXR API with the polyfill.
+      // @TODO: Remove this workaround if the major browser officially support native WebXR API
+      let overridden = false;
+      const overrideIfNeeded = () => {
+        if (overridden) { return false; }
+        if (isNativeFunction(this.global.XR)) {
+          overrideAPI(this.global);
+          overridden = true;
+          return true;
+        }
+        return false;
+      };
+      const observer = new MutationObserver(list => {
+        for (const record of list) {
+          for (const node of record.addedNodes) {
+            if (node.localName === 'script' && overrideIfNeeded()) {
+              observer.disconnect();
+              break;
+            }
+          }
+          if (overridden) { break; }
+        }
+      });
+      observer.observe(document, {subtree: true, childList: true});
+      const onLoad = event => {
+        if (!overridden) {
+          observer.disconnect();
+          overrideIfNeeded();
+        }
+        document.removeEventListener('DOMContentLoaded', onLoad);
+      };
+      document.addEventListener('DOMContentLoaded', onLoad);
     }
   }
 
@@ -84,7 +99,7 @@ export default class CustomWebXRPolyfill extends WebXRPolyfill {
   }
 }
 
-const requestXRDevice = async function (global, config) {
+const requestXRDevice = async (global, config) => {
   // resolve when receiving configuration parameters from content-script as an event
   return new Promise((resolve, reject) => {
     const callback = (event) => {
@@ -97,4 +112,16 @@ const requestXRDevice = async function (global, config) {
     };
     window.addEventListener('webxr-device-init', callback, false);
   });
+};
+
+// Easy native function detection.
+const isNativeFunction = func => {
+  return /\[native code\]/i.test(func.toString());
+};
+
+const overrideAPI = function (global) {
+  console.log('WebXR emulator extension overrides native WebXR API with polyfill.');
+  for (const className in API) {
+    global[className] = API[className];
+  }
 };
